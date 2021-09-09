42f5d2583b55e5ab928a35ecee48cb82f7195183
R0rt1z2 <rogerortizleal@gmail.com>
2021-09-09 14:01:50 +0300

    lk-payload: port to cupcake
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- lk-payload/common.h
+++ lk-payload/common.h
@@ -9,11 +9,11 @@ struct device_t {
     size_t (*write)(struct device_t *dev, void *src, uint64_t block_off, uint32_t size, uint32_t part);
 };
 
-struct device_t* (*get_device)() = (void*)0x41E22ED1;
-void (*cache_clean)(void *addr, size_t sz) = (void*)0x41E231E4;
-size_t (*video_printf)(const char *format, ...) = (void *)0x41E2790B;
+struct device_t* (*get_device)() = (void*)0x41E18ECC;
+void (*cache_clean)(void *addr, size_t sz) = (void*)0x41e191E0;
+int (*key_uber) (void) = (void*)0x41E25371;
 
-uint32_t* g_boot_mode = (uint32_t*) 0x41E5544C;
+uint32_t* g_boot_mode = (uint32_t*)0x41E3626C;
 
 #define PAYLOAD_DST 0x41000000
 #define PAYLOAD_SRC 0x80000
--- lk-payload/main.c
+++ lk-payload/main.c
@@ -5,8 +5,8 @@
 #include "common.h"
 
 void low_uart_put(int ch) {
-    volatile uint32_t *uart_reg0 = (volatile uint32_t*)0x11003014;
-    volatile uint32_t *uart_reg1 = (volatile uint32_t*)0x11003000;
+    volatile uint32_t *uart_reg0 = (volatile uint32_t*)0x11005014;
+    volatile uint32_t *uart_reg1 = (volatile uint32_t*)0x11005000;
 
     while ( !((*uart_reg0) & 0x20) )
     {}
@@ -21,24 +21,18 @@ void _putchar(char character)
     low_uart_put(character);
 }
 
-
 size_t (*original_read)(struct device_t *dev, uint64_t block_off, void *dst, uint32_t sz, uint32_t part); 
-uint32_t * boot_reason = 0;
-uint8_t * y_cable = 0;
-uint8_t boot_system = 0;
+uint64_t g_boot_a, g_boot_b, g_lk_a, g_lk_b, g_misc, g_recovery;
 
-uint64_t g_boot, g_recovery, g_lk, g_misc;
+uint32_t * boot_reason = 0;
+uint8_t boot_system = 0;
 
 size_t read_func(struct device_t *dev, uint64_t block_off, void *dst, uint32_t sz, uint32_t part) {
     printf("read_func hook\n");
     int ret = 0;
-    if (block_off == g_boot * 0x200 || block_off == g_recovery * 0x200) {
+    if (block_off == g_boot_a * 0x200 || block_off == g_recovery * 0x200) {
         printf("demangle boot image - from 0x%08X\n", __builtin_return_address(0));
 
-        if (block_off == g_boot * 0x200 && *y_cable == 1 && !boot_system && *boot_reason == 0) {
-            printf("Boot into recovery to display bootmenu");
-            block_off = g_recovery * 0x200;
-        }
         if (sz < 0x400) {
             ret = original_read(dev, block_off + 0x400, dst, sz, part);
         } else {
@@ -54,26 +48,32 @@ size_t read_func(struct device_t *dev, uint64_t block_off, void *dst, uint32_t s
 }
 
 static void parse_gpt() {
-    uint8_t raw[0x800] = { 0 };
+    uint8_t raw[0x1000] = { 0 };
     struct device_t *dev = get_device();
     dev->read(dev, 0x400, raw, sizeof(raw), USER_PART);
-    for (size_t i = 0; i < sizeof(raw) / 0x80; ++i) {
+    for (int i = 0; i < sizeof(raw) / 0x80; ++i) {
         uint8_t *ptr = &raw[i * 0x80];
         uint8_t *name = ptr + 0x38;
         uint32_t start;
         memcpy(&start, ptr + 0x20, 4);
-        if (memcmp(name, "b\x00o\x00o\x00t\x00\x00\x00", 10) == 0) {
-            printf("found boot at 0x%08X\n", start);
-            g_boot = start;
+        if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00\x61\x00\x00\x00", 14) == 0) {
+            printf("found boot_a at 0x%08X\n", start);
+            g_boot_a = start;
+        } else if (memcmp(name, "b\x00o\x00o\x00t\x00_\x00\x62\x00\x00\x00", 14) == 0) {
+            printf("found boot_b at 0x%08X\n", start);
+            g_boot_b = start;
+        } else if (memcmp(name, "l\x00k\x00_\x00\x61\x00\x00\x00", 10) == 0) {
+            printf("found lk_a at 0x%08X\n", start);
+            g_lk_a = start;
+        } else if (memcmp(name, "l\x00k\x00_\x00\x62\x00\x00\x00", 10) == 0) {
+            printf("found lk_b at 0x%08X\n", start);
+            g_lk_b = start;
+        } else if (memcmp(name, "m\x00i\x00s\x00\x63\x00\x00\x00", 10) == 0) {
+            printf("found misc at 0x%08X\n", start);
+            g_misc = start;
         } else if (memcmp(name, "r\x00\x65\x00\x63\x00o\x00v\x00\x65\x00r\x00y\x00\x00\x00", 18) == 0) {
             printf("found recovery at 0x%08X\n", start);
             g_recovery = start;
-        } else if (memcmp(name, "l\x00k\x00\x00\x00", 6) == 0) {
-            printf("found lk at 0x%08X\n", start);
-            g_lk = start;
-        } else if (memcmp(name, "M\x00I\x00S\x00\x43\x00\x00\x00", 10) == 0) {
-            printf("found misc at 0x%08X\n", start);
-            g_misc = start;
         }
     }
 }
@@ -81,6 +81,7 @@ static void parse_gpt() {
 int main() {
     int ret = 0;
     printf("This is LK-payload for mantis by xyz and k4y0z. Copyright 2019\n");
+	printf("Ported to cupcake by R0rt1z2. Copyright 2021\n");
 
     uint32_t **argptr = (void*)0x41E00020;
     uint32_t *arg = *argptr;
@@ -88,25 +89,22 @@ int main() {
 
     int fastboot = 0;
 
-    boot_reason = (uint32_t *)(*(uint32_t *)(0x41E5D904) + 272);
-    y_cable = (uint8_t *)(*(uint32_t *)(0x41E5D904) + 365);
+    boot_reason = (uint32_t *)(*(uint32_t *)(0x41E39BD0) + 272);
 
     printf("boot_reason: %u\n", *boot_reason);
-    printf("y_cable: %u\n", *y_cable);
 
     parse_gpt();
 
-    if (!g_boot || !g_recovery || !g_lk) {
+    if (!g_boot_a || !g_boot_b || !g_lk_a) {
         printf("failed to find boot, recovery or lk\n");
         printf("falling back to fastboot mode\n");
         fastboot = 1;
     }
 
-
     uint8_t bootloader_msg[0x20] = { 0 };
 
     // Need to read this before we restore the data
-    uint8_t *tmp = (void*)0x41e0062c;
+    uint8_t *tmp = (void*)0x41e00700;
 
     // microloader
     if (strncmp(tmp, "FASTBOOT_PLEASE", 15) == 0 ) {
@@ -118,7 +116,8 @@ int main() {
     // Restore the 0x41E00200-0x41E01200 range, a part of it was overwritten by microloader
     // this is way more than we actually need to restore, but it shouldn't hurt
     // we can't restore 0x41E00000-0x41E00200 as that contains important pointers
-    dev->read(dev, g_lk * 0x200 + 0x200 + 0x200, (char*)LK_BASE + 0x200, 0x1000, USER_PART); // +0x200 to skip lk header
+    dev->read(dev, g_lk_a * 0x200 + 0x200 + 0x200, (char*)LK_BASE + 0x200, 0x1000, USER_PART); // +0x200 to skip lk header
+    dev->read(dev, g_lk_b * 0x200 + 0x200 + 0x200, (char*)LK_BASE + 0x200, 0x1000, USER_PART); // +0x200 to skip lk header
 
     //Check if backup payload is present and copy if not
     dev->read(dev, BACKUP_SRC, (void*)0x45000000, PAYLOAD_SIZE, BOOT0_PART); // boot0 partition, read 512K
@@ -183,8 +182,8 @@ int main() {
       // UART flag on MISC
       if(strncmp(bootloader_msg + 0x10, "UART_PLEASE", 11) == 0) {
         // Force uart enable
-        char* disable_uart = (char*)0x41E3A3AB;
-        strcpy(disable_uart, " printk.disable_uart=0");
+        char* disable_uart = (char*)0x41e2c7f5;
+        strcpy(disable_uart, "printk.disable_uart=0");
       }
 
     }
@@ -192,29 +191,26 @@ int main() {
     uint16_t *patch;
 
     // force fastboot mode
-    if (fastboot) {
+    if (fastboot || key_uber() != 0) 
+	{
         printf("well since you're asking so nicely...\n");
+		
+		if(*g_boot_mode == 2) *o_boot_mode = 2;
+		*g_boot_mode = 99;
 
-	if(*g_boot_mode == 2) *o_boot_mode = 2;
-	
-        *g_boot_mode = 99;
-
-        video_printf("=> HACKED FASTBOOT mode: (%d) - xyz, k4y0z\n", *o_boot_mode);
-    }
-    else if(*g_boot_mode == 2) {
-        video_printf("=> RECOVERY mode...");
+        printf("=> HACKED FASTBOOT mode: (%d) - xyz, k4y0z\n", *o_boot_mode);
     }
 
     printf("g_boot_mode %u\n", *g_boot_mode);
     printf("o_boot_mode %u\n", *o_boot_mode);
 
     // device is unlocked
-    patch = (void*)0x41E01D34;
+    patch = (void*)0x41e01d1c;
     *patch++ = 0x2001; // movs r0, #1
     *patch = 0x4770;   // bx lr
 
     // amzn_verify_unlock (enable all commands)
-    patch = (void*)0x41E01EA0;
+    patch = (void*)0x41e01ad4;
     *patch++ = 0x2000; // movs r0, #0
     *patch = 0x4770;   // bx lr
 
@@ -228,7 +224,7 @@ int main() {
     cache_clean((void*)LK_BASE, LK_SIZE);
 
     printf("Jump lk\n");
-    int (*app)() = (void*)0x41E25371;
+    int (*app)() = (void*)0x41e1c6dc;
     app();
 
     while (1) {
